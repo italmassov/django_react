4919c5fed599b95cadf9e17a01d58843a3085d68
Italmassov <DBLITALMK@CORP.JTI.COM>
2018-09-12 16:39:56 +0100

    redux added

---
+++ frontend/src/actions/notes.js
@@ -0,0 +1,22 @@
+export const addNote = text => {
+	return {
+		type:'ADD_NOTE',
+		text
+	}
+	
+}
+
+export const updateNote = (id,text) => {
+	return{
+		type: 'UPDATE_NOTE',
+		id,
+		text
+	}
+}
+
+export const deleteteNote = id => {
+	return{
+		type: 'DELETE_NOTE',
+	 	id	
+	}
+}
--- frontend/src/components/PonyNote.jsx
+++ frontend/src/components/PonyNote.jsx
@@ -1,21 +1,58 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {notes} from "../actions";
 
 class PonyNote extends Component{
+
+state = {
+	text: "",
+	updateNoteId: null,
+}
+
+resetForm = () => {
+	this.setState({text: "", updateNoteId:null})
+}
+
+selectForEdit = (id) => {
+	let note = this.props.notes[id];
+	this.setState({text: note.text, updateNoteId:id});
+}
+
+submitNote = (e) => {
+	e.preventDefault();
+	if(this.state.updateNoteId == null){
+		this.prop.addNote(this.state.text);
+	} else {
+		this.prop.updateNote(this.state.updateNoteId, this.state.text);
+	}
+	this.resetForm();
+}
+
 	render(){
    	return(
 	<div>
-	<h2>Welcome to PonyNote!</h2>
+	<h1>Welcome to PonyNote!</h1>
 	<hr/>
+	<h3>Add new note</h3>
+	<form onSubmit={this.submitNote}>
+		<input 
+			value = {this.state.text}
+			placeholder = "Enter note here..."
+			onChange={(e)=>this.setState({text:e.target.value})}
+			requried />
+		<button onClick={this.resetForm}>Reset</button>
+		<input type="submit" value="Save Note" />
+	</form>
+
 	
 	<h3>Notes</h3>
 	<table>
 		<tbody>
-		{this.props.notes.map(note=>(
-			<tr>
+		{this.props.notes.map((note,id)=>(
+			<tr key={`note_${id}`}>
 			   <td>{note.text}</td>
-			   <td><button>edit</button></td>
-			   <td><button>delete</button></td>
+			   <td><button onClick={()=>this.selectForEdit(id)}>edit</button></td>
+			   <td><button onclick={() => this.props.deleteNote(id)}>delete</button></td>
 			</tr>
 		))}
 		</tbody>
@@ -33,6 +70,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch =>{
 	return{
+		addNote:(text) => {
+			dispatch(notes.addNote(text));
+		},
+		updateNote: (id, text) => {
+			dispatch(notes.addNote(id,text));
+		},
+		deleteNote: (id) => {
+			dispatch(notes.deleteNote(id));
+		},
 	}
 }
 
